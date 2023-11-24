ed8cb461c61200ebfa2a589558d1e890a155e9c9
Shivaa369 <sivaprakash030609@gmail.com>
2023-11-24 19:42:24 +0530

    tenant entity

--- backend/src/modules/tenant/tenant.entity.ts
+++ backend/src/modules/tenant/tenant.entity.ts
@@ -7,12 +7,9 @@ import { TenantSettingsEntity } from './tenant-settings.entity';
 import { UserEntity } from '../user/user.entity';
 
 
-
 @Entity({ name: 'tenants' })
 @UseDto(TenantDto)
 export class TenantEntity extends AbstractEntity<TenantDto, TenantDtoOptions> {
-
-  
 
   @Column({ nullable: true, type: 'varchar' })
   name!: string;
@@ -34,18 +31,5 @@ export class TenantEntity extends AbstractEntity<TenantDto, TenantDtoOptions> {
 
   @OneToMany(() => UserEntity, (user) => user.tenant)
   users!: UserEntity[];
-
-  // @AfterInsert()
-  // async createAdminUser() {
-  //   const adminUser = new UserEntity();
-  //   adminUser.firstName = 'Admin';
-  //   adminUser.lastName = this.name;
-  //   adminUser.role = RoleType.ORG_ADMIN;
-  //   adminUser.email = this.email; // Use the tenant's email for the admin user
-  //   adminUser.password = '1234';
-  //   adminUser.tenant = this; // Associate the user with the current tenant
-
-  //   await this.userRepository.save(adminUser);
-  // }
 
 }
