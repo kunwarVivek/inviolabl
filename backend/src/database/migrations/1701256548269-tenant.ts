3afbe8a8570033bdf7f263471979e508a0da2716
Shivaa369 <sivaprakash030609@gmail.com>
2023-11-30 22:07:07 +0530

    clerk integrate

---
+++ backend/src/database/migrations/1701256548269-tenant.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Tenant1701256548269 implements MigrationInterface {
+    name = 'Tenant1701256548269'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('ALTER TABLE "tenants" DROP CONSTRAINT "UQ_155c343439adc83ada6ee3f48be"');
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('ALTER TABLE "tenants" ADD CONSTRAINT "UQ_155c343439adc83ada6ee3f48be" UNIQUE ("email")');
+        
+    }
+
+}
