2b884594515bbabc256cca72e249ce252daa28f5
Shivaa369 <sivaprakash030609@gmail.com>
2023-12-03 13:09:13 +0530

    routes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -261,7 +261,7 @@ const Header = ({ className }: any) => {
           ) : <Link href={isTenantIncluded ? `/${tenantDetails.name}/signin` : "/signin"}><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer fill-[#5f03de] hover:shadow-2xl hover:fill-[#8364e2]" viewBox="0 0 448 512"><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z" /></svg></Link>} */}
           <SignedIn>
             {/* Mount the UserButton component */}
-            <UserButton afterSignOutUrl="https://alpha.inviolabl.io" />
+            <UserButton />
           </SignedIn>
           <SignedOut>
             {/* Signed out users get sign in button */}
