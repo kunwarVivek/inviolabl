077f4c877060f343e6e999ca9458228c32a256f5
shiva3699 <sivaprakash.k@inoesis.com>
2024-03-29 00:16:28 +0530

    sharefile

--- frontend/src/app/organization/[abc]/dashboard/page.tsx
+++ frontend/src/app/organization/[abc]/dashboard/page.tsx
@@ -52,7 +52,7 @@ const page = ({ params }) => {
 
   useEffect(() => {
     const handleScroll = () => {
-      router.replace(`http://localhost:3000/organization/${currentOrganization?.name}/dashboard`)
+      router.replace(`https://alpha.inviolabl.io/organization/${currentOrganization?.name}/dashboard`)
     };
     handleScroll()
   }, [currentOrganization]);
